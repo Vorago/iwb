a47c19b9f2d14a3cc8d0b65f4de5e329bee1df6e
Alina Annayeva <alina.annajeva@gmail.com>
2010-12-25 01:33:45 +0200

    Adding main.cpp file

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include "include/capture.hpp"
+using namespace iwb;
+
+int main(int argc, char *argv[]) {
+    int cam;
+    Capture* cpt = 0;
+
+    if (argc == 2) {
+        char *endptr;
+        cam = strtol(argv[1], &endptr, 10);
+        if (*endptr == '\0') {
+            cpt = new Capture(cam);
+        } else {
+            cpt = new Capture(argv[1]);
+        }
+    } else {
+        cam = 0;
+        cpt = new Capture(cam);
+    }
+
+    cpt->showDiff();
+    return 0;
+}
