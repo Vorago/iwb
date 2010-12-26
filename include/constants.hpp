8837f5add58fee2a4b39c5e2e65dbc4b8e60a826
Andrew Naydyonock <A.Vorago@gmail.com>
2010-12-25 17:30:14 -0800

    Added iwb::constants

---
+++ include/constants.hpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   constants.hpp
+ * Author: vorago
+ *
+ * Created on December 25, 2010, 4:46 PM
+ */
+
+#ifndef CONSTANTS_HPP
+#define	CONSTANTS_HPP
+namespace iwb {
+    namespace constants {
+        const int threshold = 50;
+    }
+}
+
+#endif	/* CONSTANTS_HPP */
+
